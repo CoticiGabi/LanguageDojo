8436134be8ee85b92f73790c4365661aefd8c289
CoticiGabi <coticigeorgegabriel@yahoo.com>
2020-04-29 14:48:36 +0300

    First commit

---
+++ LanguageDojo/LanguageDojo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LanguageDojo
+//
+//  Created by Cotici Gabriel on 4/29/20.
+//  Copyright © 2020 Cotici Gabriel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
